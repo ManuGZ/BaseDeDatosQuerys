b9a6117acfbc063e49e0fe7dc28b0564a76e85f0
CarlosG345 <115599345+CarlosG345@users.noreply.github.com>
2023-11-14 21:34:47 -0500

    Update BD_CULTURA_ENLACE.sql

--- BD_CULTURA_ENLACE.sql
+++ BD_CULTURA_ENLACE.sql
@@ -13,25 +13,25 @@ CREATE TABLE categorias (
 
 -- Table: categorias_eventos
 CREATE TABLE categorias_eventos (
-    codigo_categorias int  NOT NULL,
-    codigo_eventos int  NOT NULL,
-    CONSTRAINT categorias_eventos_pk PRIMARY KEY  (codigo_categorias,codigo_eventos)
+    codigo_categoria int  NOT NULL,
+    codigo_evento int  NOT NULL,
+    CONSTRAINT categorias_eventos_pk PRIMARY KEY  (codigo_categoria,codigo_evento)
 );
 
 -- Table: compras
 CREATE TABLE compras (
-    codigo_usuarios int  NOT NULL,
-    codigo_entradas int  NOT NULL,
+    codigo_usuario int  NOT NULL,
+    codigo_entrada int  NOT NULL,
     metodo_pago varchar(45)  NOT NULL,
     cantidad int  NOT NULL,
     fecha date  NOT NULL,
-    CONSTRAINT compras_pk PRIMARY KEY  (codigo_usuarios,codigo_entradas)
+    CONSTRAINT compras_pk PRIMARY KEY  (codigo_usuario,codigo_entrada)
 );
 
 -- Table: entradas
 CREATE TABLE entradas (
     codigo int  identity(1,1),
-    codigo_eventos int  NOT NULL,
+    codigo_evento int  NOT NULL,
 	precio money NOT NULL,
     CONSTRAINT entradas_pk PRIMARY KEY  (codigo)
 );
@@ -39,8 +39,8 @@ CREATE TABLE entradas (
 -- Table: eventos
 CREATE TABLE eventos (
     codigo int identity(1,1),
-    codigo_organizadores int  NOT NULL,
-    codigo_ubicaciones int  NOT NULL,
+    codigo_organizador int  NOT NULL,
+    codigo_ubicacion int  NOT NULL,
     nombre varchar(50)  NOT NULL,
     fecha date  NOT NULL,
     hora time  NOT NULL,
@@ -64,8 +64,8 @@ CREATE TABLE resenias (
     codigo int  identity(1,1),
     calificacion int  NOT NULL,
     comentario text  NOT NULL,
-    codigo_compras_usuarios int  NOT NULL,
-    codigo_compras_entradas int  NOT NULL,
+    codigo_compra_usuario int  NOT NULL,
+    codigo_compra_entrada int  NOT NULL,
     CONSTRAINT resenias_pk PRIMARY KEY  (codigo)
 );
 
@@ -114,43 +114,43 @@ CREATE TABLE usuarios (
 -- foreign keys
 -- Reference: categorias_eventos_categorias (table: categorias_eventos)
 ALTER TABLE categorias_eventos ADD CONSTRAINT categorias_eventos_categorias
-    FOREIGN KEY (codigo_categorias)
+    FOREIGN KEY (codigo_categoria)
     REFERENCES categorias (codigo);
 
 -- Reference: categorias_eventos_eventos (table: categorias_eventos)
 ALTER TABLE categorias_eventos ADD CONSTRAINT categorias_eventos_eventos
-    FOREIGN KEY (codigo_eventos)
+    FOREIGN KEY (codigo_evento)
     REFERENCES eventos (codigo);
 
 -- Reference: compras_entradas (table: compras)
 ALTER TABLE compras ADD CONSTRAINT compras_entradas
-    FOREIGN KEY (codigo_entradas)
+    FOREIGN KEY (codigo_entrada)
     REFERENCES entradas (codigo);
 
 -- Reference: compras_eventos (table: entradas)
 ALTER TABLE entradas ADD CONSTRAINT compras_eventos
-    FOREIGN KEY (codigo_eventos)
+    FOREIGN KEY (codigo_evento)
     REFERENCES eventos (codigo);
 
 -- Reference: compras_usuarios (table: compras)
 ALTER TABLE compras ADD CONSTRAINT compras_usuarios
-    FOREIGN KEY (codigo_usuarios)
+    FOREIGN KEY (codigo_usuario)
     REFERENCES usuarios (codigo);
 
 -- Reference: eventos_organizadores (table: eventos)
 ALTER TABLE eventos ADD CONSTRAINT eventos_organizadores
-    FOREIGN KEY (codigo_organizadores)
+    FOREIGN KEY (codigo_organizador)
     REFERENCES organizadores (codigo);
 
 -- Reference: eventos_ubicaciones (table: eventos)
 ALTER TABLE eventos ADD CONSTRAINT eventos_ubicaciones
-    FOREIGN KEY (codigo_ubicaciones)
+    FOREIGN KEY (codigo_ubicacion)
     REFERENCES ubicaciones (codigo);
 
 -- Reference: resenias_compras (table: resenias)
 ALTER TABLE resenias ADD CONSTRAINT resenias_compras
-    FOREIGN KEY (codigo_compras_usuarios,codigo_compras_entradas)
-    REFERENCES compras (codigo_usuarios,codigo_entradas);
+    FOREIGN KEY (codigo_compra_usuario,codigo_compra_entrada)
+    REFERENCES compras (codigo_usuario,codigo_entrada);
 
 -- Reference: suscripciones_tipos_suscripciones (table: suscripciones)
 ALTER TABLE suscripciones ADD CONSTRAINT suscripciones_tipos_suscripciones
